f8cb09f53885d3f87b4702570740704cc8e1a3de
senanenverli <148267397+senanenverli@users.noreply.github.com>
2023-11-15 00:37:27 +0400

    Update 22Program.cs

--- 22Program.cs
+++ 22Program.cs
@@ -9,7 +9,7 @@ namespace code_task
             try
             {
                 
-                User user1 = new User("senan", "elekberovsenan22", "Password123", 1);
+                User user1 = new User("senan", "elekberovsenan22@gmail.com", "Password123", 1);
    
                 user1.ShowInfo();
             }
